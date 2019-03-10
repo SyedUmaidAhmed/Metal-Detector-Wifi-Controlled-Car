8f18c15c5d4db7e30a50ecf2b12c4bbcbffe597c
Raspberry_Pi Pakistan <36759636+SyedUmaidAhmed@users.noreply.github.com>
2019-03-10 20:31:26 +0500

    Add files via upload

---
+++ metal_car/modifiedch.py
@@ -0,0 +1,157 @@
+import os
+import subprocess
+from subprocess import call
+import sys
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18,GPIO.IN)
+import time
+import smtplib,ssl  
+from time import sleep  
+from email.mime.multipart import MIMEMultipart  
+from email.mime.base import MIMEBase  
+from email.mime.text import MIMEText  
+from email.utils import formatdate  
+from email import encoders
+import io
+from picamera import PiCamera
+import logging
+import picamera
+import socketserver
+from threading import Condition
+from http import server
+import os
+
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
+def send_an_email():  
+    toaddr = 'syedumaidahmed96@gmail.com'      # To id 
+    me = 'techlove214@gmail.com'          # your id
+    subject = "Detection of Metal"              # Subject
+  
+    msg = MIMEMultipart()  
+    msg['Subject'] = subject  
+    msg['From'] = me  
+    msg['To'] = toaddr  
+    msg.preamble = "test "   
+    #msg.attach(MIMEText(text))  
+  
+    part = MIMEBase('application', "octet-stream")  
+    part.set_payload(open("image.jpg", "rb").read())  
+    encoders.encode_base64(part)  
+    part.add_header('Content-Disposition', 'attachment; filename="image.jpg"')   # File name and format name
+    msg.attach(part)  
+  
+    try:  
+       s = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
+       s.ehlo()  
+       s.starttls()  
+       s.ehlo()  
+       s.login(user = 'techlove214@gmail.com', password = 'syedargt12')  # User id & password
+       #s.send_message(msg)  
+       s.sendmail(me, toaddr, msg.as_string())  
+       s.quit()  
+    #except:  
+    #   print ("Error: unable to send email")    
+    except SMTPException as error:  
+          print ("Error")                # Exception
+
+
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                prev_input=1
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                        input = GPIO.input(18)
+                        if ((not prev_input) and input):
+                           print("Button pressed")
+#        camera = PiCamera()
+                           camera.capture('image.jpg', use_video_port=True)
+                           #os.system("raspistill -o image.jpg")
+                           send_an_email()
+                        prev_input = input
+                        time.sleep(0.05)
+
+                        
+
+
+
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    camera.rotation = 180
+    camera.start_recording(output, format='mjpeg')
+
+    
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
